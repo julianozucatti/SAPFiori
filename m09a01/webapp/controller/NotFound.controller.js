2553443e3ae6a9e533647ac8d08d7daf20e24951
Juliano Zucatti <zucatti@gmail.com>
2023-08-07 21:06:21 +0000

    commit 07-08-23

---
+++ m09a01/webapp/controller/NotFound.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "moovi/m09a01/controller/BaseController"
+    
+],
+    function (Controller){
+        return Controller.extend("moovi.m09a01.controller.NotFound",{
+            onInit: function(){
+
+            }
+        });
+    });
---
+++ m09a01/webapp/controller/View1.controller.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+    "moovi/m09a01/controller/BaseController"
+    
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("moovi.m09a01.controller.View1", {
+            onInit: function () {
+
+            },
+
+            onListItemPress : function(oEvent){
+                var oItem, oCtx;
+
+                oItem = oEvent.getSource();
+                oCtx = oItem.getBindingContext();
+
+                this.getRouter().navTo("RouteCompanyDetail",{
+                    carrid : oCtx.getProperty("Carrid")
+                });
+            }
+        });
+    });
